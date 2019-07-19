3da779469419b8eca72381b6853b0ab38930eff7
Eric-Cen <scen01@gmail.com>
2019-07-18 23:39:06 -0400

    feat: added GameViewModel

--- app/src/main/java/com/mcarving/game24/GameActivity.kt
+++ app/src/main/java/com/mcarving/game24/GameActivity.kt
@@ -6,16 +6,18 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.mcarving.game24.cards.Card
 import com.mcarving.game24.cards.CardView
 import kotlinx.android.synthetic.main.activity_game.*
+import java.lang.Exception
 
 
 class GameActivity : AppCompatActivity() {
-    private lateinit var nameOne : String
+    private lateinit var nameOne: String
 
-    private lateinit var nameTwo : String
+    private lateinit var nameTwo: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,10 +26,12 @@ class GameActivity : AppCompatActivity() {
         // remove app title bar
         supportActionBar?.apply { hide() }
 
-
-
         setupPlayerNames()
-        generateFourRandomCards()
+
+        val viewModel = ViewModelProviders.of(this)
+            .get(GameViewModel::class.java)
+
+        displayFourCards(viewModel.getFourCards())
 
         btn_answer1.setOnClickListener {
             startAnswerActivity(nameOne)
@@ -39,13 +43,79 @@ class GameActivity : AppCompatActivity() {
 
         }
 
-        val viewModel = ViewModelProviders.of(this)
-            .get(GameViewModel::class.java)
+        btn_pass1.setOnClickListener {
+            viewModel.setupPassByPlayer(GameViewModel.Player.ONE, true)
+            it.isEnabled = false
+        }
 
-        viewModel.shuffleDeck()
+        val playerOneObserver = Observer<Boolean> { player_one_passes ->
+            // check if player 2 passes, if yes, display another 4 cards
+            if (viewModel.getPassByPlayer(GameViewModel.Player.TWO) == true) {
+                setPassButtonText("2/2 Passes")
+
+                displayFourCards(viewModel.getFourCards())
+
+                clearPassText()
+                viewModel.setupPassByPlayer(GameViewModel.Player.ONE, false)
+                viewModel.setupPassByPlayer(GameViewModel.Player.TWO, false)
+            } else if (viewModel.getPassByPlayer(GameViewModel.Player.ONE) == false) {
+                setPassButtonText("0/2 Pass")
+
+            } else {
+                setPassButtonText("1/2 Pass")
+
+            }
+        }
+
+        viewModel.player_one_pass.observe(this, playerOneObserver)
+
+        btn_pass2.setOnClickListener {
+            viewModel.setupPassByPlayer(GameViewModel.Player.TWO, true)
+            it.isEnabled = false
+        }
+
+
+        val playerTwoObserver = Observer<Boolean> { player_two_passes ->
+            // check if player 2 passes, if yes, display another 4 cards
+            if (viewModel.getPassByPlayer(GameViewModel.Player.ONE) == true) {
+                setPassButtonText("2/2 Passes")
+
+                displayFourCards(viewModel.getFourCards())
+                clearPassText()
+
+
+
+                viewModel.setupPassByPlayer(GameViewModel.Player.ONE, false)
+                viewModel.setupPassByPlayer(GameViewModel.Player.TWO, false)
+            } else if (viewModel.getPassByPlayer(GameViewModel.Player.TWO) == false) {
+
+                setPassButtonText("0/2 Pass")
+
+            } else {
+                setPassButtonText("1/2 Pass")
+            }
+        }
+
+        viewModel.player_two_pass.observe(this, playerTwoObserver)
     }
 
-    fun startAnswerActivity(playerName: String){
+    fun setPassButtonText(msg: String) {
+        btn_pass1.text = msg
+        btn_pass2.text = msg
+
+    }
+
+    fun clearPassText(){
+        btn_pass1.text = "0/2 Pass"
+        btn_pass2.text = "0/2 Pass"
+
+        btn_pass1.isEnabled = true
+        btn_pass2.isEnabled = true
+
+
+    }
+
+    fun startAnswerActivity(playerName: String) {
 
         val intent = Intent(this, AnswerActivity::class.java).apply {
             putExtra(AnswerActivity.EXTRA_PLAYER_NAME, playerName)
@@ -55,24 +125,20 @@ class GameActivity : AppCompatActivity() {
 
     }
 
-    fun generateFourRandomCards(){
-        // retrieve 4 cards from GameViewModel
 
-        val card1 = Card.DECK[0]
-        val card2 = Card.DECK[1]
-        val card3 = Card.DECK[2]
-        val card4 = Card.DECK[51]
-        //val cardView1 = CardView(applicationContext)
-//        val cardv = findViewById<CardView>(R.id.view1)
-//        cardv.bind(card1)
-        view1.bind(card1)
-        view2.bind(card2)
-        view3.bind(card3)
-        view4.bind(card4)
+    fun displayFourCards(cards : List<Card>){
+        if(cards.size == 4){
+            view1.bind(cards[0])
+            view2.bind(cards[1])
+            view3.bind(cards[2])
+            view4.bind(cards[3])
+        } else {
+            throw Exception("Invalid card size")
+        }
 
     }
 
-    fun setupPlayerNames(){
+    fun setupPlayerNames() {
         nameOne = intent.getStringExtra(EXTRA_PLAYER_ONE)
         nameTwo = intent.getStringExtra(EXTRA_PLAYER_TWO)
         tv_player_one.text = nameOne
--- app/src/main/java/com/mcarving/game24/GameViewModel.kt
+++ app/src/main/java/com/mcarving/game24/GameViewModel.kt
@@ -1,39 +1,166 @@
 package com.mcarving.game24
 
 import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.mcarving.game24.cards.Card
 import kotlin.random.Random
 
 class GameViewModel : ViewModel() {
+    enum class Player {
+        ONE, TWO
+    }
+
+    val player_one_pass = MutableLiveData<Boolean>()
+    var player_two_pass = MutableLiveData<Boolean>()
 
     // list of poker cards for player 1
+    private lateinit var playerOneCards : MutableList<Card>
+    private var currentPointerOne = -1
 
     // list of poker cards for player 2
+    private lateinit var playerTwoCards : MutableList<Card>
+    private var currentPointerTwo = -1
 
+    init {
+        player_one_pass.value = false
+        player_two_pass.value = false
+
+        Log.d("CardViewModel", "shuffleDeck: ${Card.DECK.toString()}")
+        // initial shuffle cards
+        val cardDeck = shuffleCards(Card.DECK)
+
+        val(a, b) = cardDeck.partition {acard ->
+            cardDeck.indexOf(acard) % 2 == 0
+        }
+
+        Log.d("CardViewModel", "shuffleDeck: ${a.toString()}")
+        Log.d("CardViewModel", "shuffleDeck: ${a.size}")
+
+        Log.d("CardViewModel", "shuffleDeck: ${b.toString()}")
+        Log.d("CardViewModel", "shuffleDeck: ${b.size}")
+
+        playerOneCards = a.toMutableList()
+        playerTwoCards = b.toMutableList()
+
+        reshuffleCardsInHand()
+    }
 
     /******
      * Knuth Shuffle
      */
 
-    fun shuffleDeck()
-    {
-        var cardDeck = Card.DECK.toMutableList()
+    fun shuffleCards(cards : List<Card>) : MutableList<Card> {
+        var shuffledCards = cards.toMutableList()
         val rnd = Random
 
-        for(x in (cardDeck.size-1) downTo 1){
+        for(x in (shuffledCards.size-1) downTo 1){
             val index = rnd.nextInt(x + 1)
             // Simple swap
-            val temp = cardDeck[index]
-            cardDeck[index] = cardDeck[x]
-            cardDeck[x] = temp
+            val temp = shuffledCards[index]
+            shuffledCards[index] = shuffledCards[x]
+            shuffledCards[x] = temp
         }
 
-        Log.d("CardViewModel", "shuffleDeck: ${Card.DECK.toString()}")
-        Log.d("CardViewModel", "shuffleDeck: ${cardDeck.toString()}")
+
+        Log.d("CardViewModel", "shuffleDeck before: ${cards.toString()}")
+        Log.d("CardViewModel", "shuffleDeck: after: ${shuffledCards.toString()}")
+
+        return shuffledCards
     }
 
+    fun reshuffleCardsInHand(){
+        if(playerOneCards.size > 1){
+            playerOneCards = shuffleCards(playerOneCards)
+            Log.d("CardViewModel", "reshuffled player one: ${playerOneCards.toString()}")
+
+        }
+
+        if(playerTwoCards.size > 1){
+
+            playerTwoCards = shuffleCards(playerTwoCards)
+            Log.d("CardViewModel", "reshuffled player Two: ${playerTwoCards.toString()}")
 
 
+        }
+
+
+    }
+
+    fun updateResult(cardsChanged : List<Card>, winner : Player, loser : Player){
+        addCards(cardsChanged, loser)
+        removeCards(cardsChanged, winner)
+    }
+
+    // if the user loses, the user collects the two cards from other player
+    fun addCards(addList : List<Card>, who : Player){
+        for(card in addList){
+            when(who){
+                Player.ONE -> playerOneCards.add(card)
+                Player.TWO -> playerTwoCards.add(card)
+            }
+        }
+    }
+
+    // if the user wins, the user gives aways the two cards
+    fun removeCards(removeList : List<Card>, who : Player ){
+        for (card in removeList){
+            when(who){
+                Player.ONE -> playerOneCards.remove(card)
+                Player.TWO -> playerTwoCards.remove(card)
+            }
+        }
+
+    }
+
+    fun setupPassByPlayer(player : Player, status : Boolean){
+        when (player){
+            Player.ONE -> player_one_pass.value = status
+            Player.TWO -> player_two_pass.value = status
+        }
+    }
+
+    fun getPassByPlayer(player: Player) : Boolean?{
+        when(player){
+            Player.ONE -> return player_one_pass.value
+            Player.TWO -> return player_two_pass.value
+        }
+    }
+
+    fun getCardSize(player: Player) : Int {
+        when(player){
+            Player.ONE -> return playerOneCards.size
+            Player.TWO -> return playerTwoCards.size
+        }
+    }
+
+    fun getFourCards() : List<Card>{
+
+        if(currentPointerOne == (playerOneCards.size-1)){
+            playerOneCards = shuffleCards(playerOneCards)
+            currentPointerOne = -1
+        }
+
+        if(currentPointerTwo == (playerTwoCards.size-1)){
+            playerTwoCards = shuffleCards(playerTwoCards)
+            currentPointerTwo = -1
+
+        }
+        val tempList = MutableList<Card>(4){Card.DECK[0]}
+        if(playerOneCards.size >= 2 && playerTwoCards.size >= 2){
+            tempList[0] = playerOneCards[++currentPointerOne]
+            tempList[1] = playerOneCards[++currentPointerOne]
+
+            tempList[2] = playerTwoCards[++currentPointerTwo]
+            tempList[3] = playerTwoCards[++currentPointerTwo]
+        } else if(playerOneCards.size < 2 && playerTwoCards.size >= 2){
+
+        } else if(playerOneCards.size >= 2 && playerTwoCards.size < 2){
+
+        }
+
+        return tempList
+    }
 
 }
