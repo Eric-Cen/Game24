0ff4cbba9230f74c39d69fa4bafbdef7a055a180
Eric-Cen <scen01@gmail.com>
2019-07-24 23:40:58 -0400

    feat: worked on implementing ROOM database

--- app/src/main/java/com/mcarving/game24/data/CardsDatabase.kt
+++ app/src/main/java/com/mcarving/game24/data/CardsDatabase.kt
@@ -3,6 +3,7 @@ package com.mcarving.game24.data
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
+import com.mcarving.game24.Utils
 import com.mcarving.game24.cards.Card
 import java.util.concurrent.Executors
 
@@ -19,7 +20,7 @@ abstract class CardsDatabase : RoomDatabase(){
         Executors.newSingleThreadScheduledExecutor().execute {
             beginTransaction()
             try {
-                // reset
+                // reset card data for the players
                 for(i in 0..1) {
                     playCardDao().setCardsInHand(i, arrayListOf<Card>())
                     playCardDao().setTwoCards(i, arrayListOf<Card>())
@@ -29,7 +30,25 @@ abstract class CardsDatabase : RoomDatabase(){
                 }
 
                 // initialize data
+            } catch (){
+                val cardDeck = Utils.shuffleCards(Card.DECK)
+                val(a, b) = cardDeck.partition { acard ->
+                    cardDeck.indexOf(acard) % 2 == 0
+                }
+
+                // set initial data for player 1
+                playCardDao().setCardsInHand(0, a)
+                playCardDao().setTwoCards(0, arrayListOf(a[0], a[1]))
+                playCardDao().setSelectedCardInex(0, 1)
+
+                // set initial data for player 2
+                playCardDao().setCardsInHand(1, b)
+                playCardDao().setTwoCards(1, listOf(b[0], b[1]))
+                playCardDao().setSelectedCardInex(1, 1)
+
             }
         }
     }
+
+
 }
